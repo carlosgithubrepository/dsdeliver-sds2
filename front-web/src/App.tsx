a528e554b951c98ae3e5bd88affc27d2b3fa186a
Carlos <carlos.developer.job@gmail.com>
2021-01-09 13:23:18 -0300

    Add Home Page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
-import NavBar from './Navbar/Index';
+import Home from './Home';
+import NavBar from './Navbar';
 
 function App() {
   return  (
-  <div>
+  <>
    <NavBar/>
-  </div> 
+   <Home/>
+  </> 
   )
 }
 
