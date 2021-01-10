4ec7adebe42293183d95c548c4dd5c1133c9e5ce
Carlos <carlos.developer.job@gmail.com>
2021-01-10 13:24:02 -0300

    Delete import Navbar

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -1,12 +1,10 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./Home";
-import NavBar from "./Navbar";
 import Orders from "./Orders";
 
 function Routes(){
     return(
         <BrowserRouter>
-        <NavBar/>
         <Switch>
             <Route path="/orders">
                 <Orders/>
