c6e288922208e4c4464cf621f590ba8e9c530bb0
Carlos <carlos.developer.job@gmail.com>
2021-01-10 12:05:42 -0300

    Rename routes.tsx

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,7 +1,7 @@
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
-import Routes from './Routes';
+import Routes from './routes';
 
 function App() {
   return  (
